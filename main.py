9a90568ad7556e68d9965ede01c9c63e76c7aca4
Weegor Martins <144939084+WeegorMartins@users.noreply.github.com>
2025-03-26 14:46:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,27 +5,31 @@ import pandas as pd
 
 app = FastAPI(title="API de Churn Prediction", version="1.0")
 
-model = joblib.load("modelo_pipeline_treinado.pkl")  # Suba esse arquivo depois
+# Carregar pipeline treinado
+model = joblib.load("modelo_pipeline_treinado.pkl")
 
+# Modelo dos dados esperados
 class Cliente(BaseModel):
+    id_cliente: str
     idade: int
-    tempo_como_cliente_meses: int
-    renda_mensal: float
-    engajamento_mensal: float
-    ultima_atividade_dias: int
-    interacoes_com_pushs: int
-    numero_contatos_suporte: int
-    ticket_medio: float
-    nps: float
-    uso_cashback: int
-    bonus_recebido: int
-    score_interno: float
-    plano_atual: str
-    canal_aquisicao: str
-    categoria_profissional: str
-    cidade: str
-    dispositivo_preferido: str
     sexo: str
+    cidade: str
+    plano_atual: str
+    tempo_como_cliente_meses: int
+    engajamento_mensal: float
+    nps: int
+    numero_contatos_suporte: int
+    ultima_atividade_dias: int
+    qtde_produtos_ativos: int
+    renda_mensal: float
+    canal_aquisicao: str
+    dispositivo_preferido: str
+    categoria_profissional: str
+    score_interno: float
+    uso_cashback: float
+    bonus_recebido: int
+    interacoes_com_pushs: int
+    ticket_medio: float
 
 @app.get("/")
 def home():
@@ -33,10 +37,13 @@ def home():
 
 @app.post("/predict")
 def predict(cliente: Cliente):
-    input_data = pd.DataFrame([cliente.dict()])
-    proba = model.predict_proba(input_data)[0][1]
-    pred = model.predict(input_data)[0]
-    return {
-        "probabilidade_cancelamento": round(float(proba), 4),
-        "vai_cancelar": bool(pred)
-    }
+    try:
+        input_df = pd.DataFrame([cliente.dict()])
+        proba = model.predict_proba(input_df)[0][1]
+        pred = model.predict(input_df)[0]
+        return {
+            "probabilidade_cancelamento": round(float(proba), 4),
+            "vai_cancelar": bool(pred)
+        }
+    except Exception as e:
+        return {"erro": str(e)}
